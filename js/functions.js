1294c79e84aa99815b3e51882ac426796034c0f2
杨源吉 <5741912+yang_yuan_ji@user.noreply.gitee.com>
2021-01-11 15:04:18 +0800

    2

--- js/functions.js
+++ js/functions.js
@@ -110,7 +110,7 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "<span class=\"digit\">" + 2020 + "</span> years <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
+	var result = "<span class=\"digit\">" + 2021 + "</span> years <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
 	$("#elapseClock").html(result);
 }
 
